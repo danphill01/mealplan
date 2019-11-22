d7674c19d6aef9a6aa1f42d55cacc3f2ee442c2b
Daniel Phillips <dphillips@catalyte.io>
2019-11-21 16:19:16 -0800

    Changed card style to columns

--- src/app/components/meal-list/meal-list.component.ts
+++ src/app/components/meal-list/meal-list.component.ts
@@ -4,7 +4,6 @@ import { MealService } from 'src/app/services/meal.service';
 @Component({
   selector: 'app-meal-list',
   templateUrl: './meal-list.component.html',
-  styleUrls: ['./meal-list.component.css']
 })
 export class MealListComponent implements OnInit {
 
